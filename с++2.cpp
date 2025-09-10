ce8e9b6e76a1839551aeee8b5604e26e90722a2e
Rasesoni <145288805+Rasesoni@users.noreply.github.com>
2025-09-10 20:29:27 +0300

    Create с++2.cpp

---
+++ с++2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char list[6][2] = {"a", "1", "b", "2", "c", "3"};
+    for (int i = 0; i < 6; i += 2) {
+        cout << list[i] << " ";
+    }
+    cout << std::endl;
+    for (int i = 1; i < 6; i += 2) {
+        cout << list[i] << " ";
+    }
+    return 0;
+}
