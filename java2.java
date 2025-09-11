e2a7d9c2eb28a0a7e2ca01aecaad931cbaf03f21
Rasesoni <145288805+Rasesoni@users.noreply.github.com>
2025-09-11 11:52:03 +0300

    Create java2.java

---
+++ java2.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    ArrayList<String> students = new ArrayList<String>();
+    students.add("a");
+    students.add("1");
+    students.add("b");
+    students.add("2");
+    students.add("c");
+    students.add("3");
+    System.out.println(students);
+    
+
+  }
+}
