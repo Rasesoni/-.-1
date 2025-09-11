c0680b728c5969987462cb420bc0f5f7538e5f6d
Rasesoni <145288805+Rasesoni@users.noreply.github.com>
2025-09-11 18:04:37 +0300

    Update java2.java

--- java2.java
+++ java2.java
@@ -1,19 +1,27 @@
 import java.util.ArrayList;
-
-public class Main {
-
-  public static void main(String[] args) {
-
-    ArrayList<String> students = new ArrayList<String>();
-    students.add("a");
-    students.add("1");
-    students.add("b");
-    students.add("2");
-    students.add("c");
-    students.add("3");
-    System.out.println(students);
-    students.clear()
-    
-
-  }
+import java.util.List;
+ class SplitListExample {
+    public static void main(String[] args) {
+        ArrayList<String> it= new ArrayList<>();
+        it.add("a");
+        it.add("1");
+        it.add("b");
+        it.add("2");
+        it.add("c");
+        it.add("3");
+        List<String> let = new ArrayList<>();
+        List<String> num = new ArrayList<>();
+        for (String item : it) {
+            if (item.matches("[a-zA-Z]")) {
+                let.add(item);
+            } else if (item.matches("\\d")) {
+                num.add(item);
+            }
+        }
+        it.clear();
+        it = null;
+        System.out.println(let);
+        System.out.println(num);
+    }
 }
+
